346676dd15497857b4f53cca2ca8acb0b3624fcd
Jack <52207916+jackkuma@users.noreply.github.com>
2020-01-05 22:35:08 +0800

    js export

---
+++ js/login.js
@@ -0,0 +1,36 @@
+    window.addEventListener('load', () => {
+      inputSpan.style.display = 'none';
+      if (typeof(Storage)=='undefined'){
+        alert('使用的瀏覽器不支援Web Storage T^T');
+    } else {
+      if (localStorage.userName) {
+        if (!sessionStorage.counter) {
+          sessionStorage.counter = 1;
+        } else {
+          sessionStorage.counter++;
+        }
+        btnLogin.style.display = 'none';
+        showLocalStorage.innerHTML = localStorage.userName + '您好，這是您第' + sessionStorage.counter + '次來到本畫面';
+      }
+      btnLogin.addEventListener('click', login);
+      btnSend.addEventListener('click', sendOk);
+      btnLogout.addEventListener('click', clearStorage);
+    }
+    })
+
+    function sendOk() {
+      localStorage.userName = myName.value;
+      location.reload();
+    }
+  
+    function login() {
+      inputSpan.style.display = '';
+    }
+
+    function clearStorage() {
+      localStorage.clear();
+      sessionStorage.clear();
+      showLocalStorage.innerHTML = '您已成功登出!!';
+      btnLogin.style.display = '';
+      inputSpan.style.display = '';
+    }
